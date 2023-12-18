af13dae7df845eb95e173ff8b57c72681201721d
Md Areeb Ansar <mdareebansar@gmail.com>
2023-12-19 00:40:53 +0530

    Configured nodemailer and gave it a try

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,4 +3,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL,
+  GMAIL_PASS: process.env.GMAIL_PASS,
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 
-const { ServerConfig, Logger } = require("./config");
+const { ServerConfig, Logger, EmailConfig } = require("./config");
+
 const apiRoutes = require("./routes");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Server started at PORT: ${ServerConfig.PORT}`);
   Logger.info("Server up and running.");
+  const response = await EmailConfig.sendMail({
+    to: "mdareebansar@gmail.com",
+    subject: "Test",
+    text: "Feedback",
+  });
+  console.log(response);
 });
